42a6c7eef4686636a97ba2b707e57904b783f2df
Victor Lee <b9602025@gmail.com>
2015-05-12 22:21:00 +0800

    Initiate The Project

---
+++ OAPhotoNote/OAPhotoNote/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  OAPhotoNote
+//
+//  Created by lee on 2015/5/12.
+//  Copyright (c) 2015年 Lee Chung-Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ OAPhotoNote/OAPhotoNote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OAPhotoNote
+//
+//  Created by lee on 2015/5/12.
+//  Copyright (c) 2015年 Lee Chung-Yun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
